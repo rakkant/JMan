5289a4ec880f5f83b1531660c60b351701793e59
Rakkan Tiasakul <rakkantia@gmail.com>
2016-05-09 17:24:31 +0700

    edited Piece.java

--- Piece.java
+++ Piece.java
@@ -8,36 +8,52 @@ public abstract class Piece{
     public static final int WALKER= 2; // piece is a walker
     public static final int PILLAR= 3; // piece is a pillar
     
+    private int type ;
+    private int X ;
+    private int Y ;
+    private Color color ;
+    private boolean acted ;
+    private Map map;
+    
     /** Constructor: a Piece on Map m with type t.
         Precondition: type is one of the four constants of this class. */
     public Piece(int t, Map m){
-        
+    	this.type = t;
+    	this.map = m;
     }
     
     /** = this piece's type: (one of the four constants of this class. */
     public int getType(){
-        return 0;
+        return  type ;
     }
     
     /** = this piece's x location. */
     public int getX(){
-        return 0;
+        return X;
     }
     
     /** = this piece's y location. */
     public int getY(){
-        return 0;
+        return Y;
     }
     
     /** = this piece's color. */
     public Color getColor(){
-        return null;
+        return color;
     }
     
     /** = the color of this piece, as a capitalized word (e.g. "Red").
         = the empty string if it is not one of red, green, yellow, or white*/
     public String getColorWord() {
-        // complete this
+        if (getColor().equals(Color.RED)){
+        	return "Red";
+        }else if (getColor().equals(Color.GREEN)){
+        	return "Green";
+        }else if (getColor().equals(Color.YELLOW)){
+        	return "Yellow";
+        }else if(getColor().equals(Color.WHITE)){
+        	return "White";
+        }
         return "";
     }
     
@@ -53,30 +69,30 @@ public abstract class Piece{
     
     /** Set this piece's x location to x. */
     public void setX(int x){
-        
+        this.X = x;
     }
     
     /** Set this piece's y location to y. */
     public void setY(int y){
-        
+        this.Y = y;
     }
     
     /** Set this piece's color to the c.
         Precondition: c is Color.WHITE and this is a block,OR
         c is Color.RED, Color.GREEN, or Color.YELLOW. */
     public void setColor(Color c){
-        
+        this.color = c;
     }
     
     /** Set the state of this piece to the value of acted.*/
     public void setActed(boolean acted){
-        
+       this.acted = acted;
     }
     
     /** Make this piece take one action. What a piece does on
         its action is defined in the assignment. */
     public abstract void act();
-    
+  
     /** = a random integer in the range lo..hi, with all integers in
           the range being equally likely. 
           Precondition lo < hi. */
